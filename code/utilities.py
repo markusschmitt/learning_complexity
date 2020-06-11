46d8a836e6f5816bb84946dd7f17457bfa2cc759
Markus <markus.schmitt@berkeley.edu>
2020-06-10 17:31:23 -0700

    Fixed sign error in inverse KL...

--- code/utilities.py
+++ code/utilities.py
@@ -2,4 +2,4 @@ import physics
 from jax import numpy as jnp
 
 def compute_inverse_KL(samples, logProbabilities, L,T,F,bc="obc"):
-    return (-jnp.sum(-physics.energies(samples,L,bc)/T-F) + jnp.sum(logProbabilities)) / 0.6931471806
+    return (-jnp.sum(-physics.energies(samples,L,bc)/T+F) + jnp.sum(logProbabilities)) / 0.6931471806
