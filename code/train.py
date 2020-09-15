c16564fbc75204adb04d48dccc2af35a250bbb1b
Markus <31160708+markusschmitt@users.noreply.github.com>
2020-09-15 02:07:07 +0200

    Forgot to remove exit()...

--- code/train.py
+++ code/train.py
@@ -131,7 +131,6 @@ MSq=np.sum(testData,axis=(1,2))
 MSq=(MSq*MSq)
 tauData=MSq[:r*R].reshape(r,R)
 print("Autocorrelation time from test data squared magn.:", R*np.var(np.mean(tauData, axis=1))/np.var(MSq))
-exit()
 
 # RNN works with spin up/down = 1/0
 trainData[trainData==-1]=0
